66fa269ccb82298b8734182c50c369590303a9cc
Lee Howorko <lhoworko@gmail.com>
2019-11-08 21:22:54 -0700

    Add startup led show

--- wordclock.ino
+++ wordclock.ino
@@ -1,62 +1,64 @@
-const int button1Pin = 2;
-const int button2Pin = 3;
-const int ledPin = 13;
+#include <FastLED.h>
+
+#define BUTTON_PIN 3
+#define LED_PIN 13
+
+#define LED_TYPE WS2811
+#define COLOR_ORDER GRB
+#define BRIGHTNESS 64
+
+#define WIDTH 13
+#define HEIGHT 8
+#define NUM_LEDS (WIDTH * HEIGHT)
+
+#define STARTUP_SECONDS 3
+
+CRGB leds[NUM_LEDS];
 
 int ledState = HIGH;
-
-int button1State;
-int button2State;
-
-int lastButton1State = LOW;
-int lastButton2State = LOW;
+int buttonState;
+int lastButtonState = LOW;
 
 unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
 unsigned long debounceDelay = 100;   // the debounce time; increase if the output flickers
 
 void setup() {
-  pinMode(button1Pin, INPUT);
-  pinMode(button2Pin, INPUT);
-  pinMode(ledPin, OUTPUT);
+  delay(3000);
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(BRIGHTNESS);
+  
+  pinMode(BUTTON_PIN, INPUT);
 
-  digitalWrite(ledPin, ledState);
-
-  Serial.begin(9600);
+  uint32_t start_time = millis();
+  for (uint32_t ms = millis(); ms < (start_time + (STARTUP_SECONDS * 1000)); ms = millis()) {
+    int32_t yHueDelta32 = ((int32_t)cos16(ms * (27/1)) * (350 / WIDTH));
+    int32_t xHueDelta32 = ((int32_t)cos16(ms * (39/1)) * (310 / HEIGHT));
+    drawFrame(ms / 65536, yHueDelta32 / 32768, xHueDelta32 / 32768);
+    
+    FastLED.setBrightness(BRIGHTNESS);
+    FastLED.show();
+  }
 }
 
 void loop() {
-  int pin1Reading = digitalRead(button1Pin);
-  int pin2Reading = digitalRead(button2Pin);
+  int buttonReading = digitalRead(BUTTON_PIN);
 
-  if (pin1Reading == HIGH && pin2Reading == HIGH) {
-    // Don't bother handling when both buttons are high.
-    return;
+  if (buttonPressed(buttonReading)) {
+
   }
-
-  if (buttonPressed(pin1Reading, &button1State, &lastButton1State)) {
-    ledState = !ledState;
-    Serial.write("Button 1 clicked\n");
-  }
-
-  if (buttonPressed(pin2Reading, &button2State, &lastButton2State)) {
-    ledState = !ledState;
-    Serial.write("Button 2 clicked\n");
-  }
-
-  digitalWrite(ledPin, ledState);
 }
 
-boolean buttonPressed(int pinReading, int *buttonState, int *lastButtonState) {
+boolean buttonPressed(int pinReading) {
   // Debouncing logic from the example Arduino Debounce sketch.
   boolean result = false;
   
-  if (pinReading != *lastButtonState) {
+  if (pinReading != lastButtonState) {
     lastDebounceTime = millis();
   }
   
   if ((millis() - lastDebounceTime) > debounceDelay) {
-    if (pinReading != *buttonState) {
-      Serial.write("Button\n");
-      *buttonState = pinReading;
+    if (pinReading != buttonState) {
+      buttonState = pinReading;
 
       if (pinReading == HIGH) {
         result = true;
@@ -64,6 +66,33 @@ boolean buttonPressed(int pinReading, int *buttonState, int *lastButtonState) {
     }
   }
   
-  *lastButtonState = pinReading;
+  lastButtonState = pinReading;
   return result;
+}
+
+void drawFrame(byte startHue8, int8_t yHueDelta8, int8_t xHueDelta8) {
+  byte lineStartHue = startHue8;
+  
+  for( byte y = 0; y < HEIGHT; y++) {
+    lineStartHue += yHueDelta8;
+    byte pixelHue = lineStartHue;    
+      
+    for (byte x = 0; x < WIDTH; x++) {
+      pixelHue += xHueDelta8;
+      leds[xy(x, y)]  = CHSV(pixelHue, 255, 255);
+    }
+  }
+}
+
+uint16_t xy(uint8_t x, uint8_t y) {
+  uint16_t i;
+  
+  if (y & 0x01) {
+    uint8_t reverseX = (WIDTH - 1) - x;
+    i = (y * WIDTH) + reverseX;
+  } else {
+    i = (y * WIDTH) + x;
+  }
+  
+  return i;
 }
