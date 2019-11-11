65c1ac2f60682b02215c7ab199df00686ab55716
Lee Howorko <lhoworko@gmail.com>
2019-11-11 13:53:26 -0700

    Organize the code better

--- ledGrid.h
+++ ledGrid.h
@@ -12,8 +12,8 @@ int TO[] {1, 4, 3, 2};
 int OCLOCK[] {1, 7, 7, 6};
 
 // Minutes
-int FIVE[] {1, 0, 2, 4};
-int TEN[] {1, 6, 0, 3};
+int FIVEM[] {1, 0, 2, 4};
+int TENM[] {1, 6, 0, 3};
 int QUARTER[] {1, 0, 1, 6};
 int TWENTY[] {1, 7, 1, 6};
 int TWENTYFIVE[] {2, 7, 1, 6, 0, 2, 4};
@@ -24,12 +24,12 @@ int ONE[] {1, 7, 3, 3};
 int TWO[] {1, 10, 3, 3};
 int THREE[] {1, 0, 4, 5};
 int FOUR[] {1, 5, 4, 4};
-int FIVE[] {1, 9, 4, 4};
+int FIVEH[] {1, 9, 4, 4};
 int SIX[] {1, 0, 5, 3};
 int SEVEN[] {1, 3, 5, 5};
 int EIGHT[] {1, 8, 5, 5};
 int NINE[] {1, 0, 6, 4};
-int TEN[] {1, 4, 6, 3};
+int TENH[] {1, 4, 6, 3};
 int ELEVEN[] {1, 7, 6, 6};
 int TWELVE[] {1, 0, 7, 6};
 #endif
--- wordclock.ino
+++ wordclock.ino
@@ -24,22 +24,13 @@ unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
 unsigned long debounceDelay = 100;   // the debounce time; increase if the output flickers
 
 void setup() {
+  pinMode(BUTTON_PIN, INPUT);
+
   delay(3000);
   FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.setBrightness(BRIGHTNESS);
   
-  pinMode(BUTTON_PIN, INPUT);
-  Serial.begin(9600);
-
-  uint32_t start_time = millis();
-  for (uint32_t ms = millis(); ms < (start_time + (STARTUP_SECONDS * 1000)); ms = millis()) {
-    int32_t yHueDelta32 = ((int32_t)cos16(ms * (27/1)) * (350 / WIDTH));
-    int32_t xHueDelta32 = ((int32_t)cos16(ms * (39/1)) * (310 / HEIGHT));
-    drawFrame(ms / 65536, yHueDelta32 / 32768, xHueDelta32 / 32768);
-    
-    FastLED.setBrightness(BRIGHTNESS);
-    FastLED.show();
-  }
+  bootAnimation();
 
   resetClock();
 
@@ -50,10 +41,6 @@ void setup() {
 }
 
 void loop() {
-
-
-
-  
   int buttonReading = digitalRead(BUTTON_PIN);
 
   if (buttonPressed(buttonReading)) {
@@ -61,6 +48,7 @@ void loop() {
   }
 }
 
+// Returns true when the button is initially pressed. Only returns true once, holding down does nothing.
 boolean buttonPressed(int pinReading) {
   // Debouncing logic from the example Arduino Debounce sketch.
   boolean result = false;
@@ -83,6 +71,19 @@ boolean buttonPressed(int pinReading) {
   return result;
 }
 
+// Plays the bootup animation
+void bootAnimation() {
+  uint32_t start_time = millis();
+  for (uint32_t ms = millis(); ms < (start_time + (STARTUP_SECONDS * 1000)); ms = millis()) {
+    int32_t yHueDelta32 = ((int32_t)cos16(ms * (27/1)) * (350 / WIDTH));
+    int32_t xHueDelta32 = ((int32_t)cos16(ms * (39/1)) * (310 / HEIGHT));
+    drawFrame(ms / 65536, yHueDelta32 / 32768, xHueDelta32 / 32768);
+
+    FastLED.show();
+  }
+}
+
+// Draw a single frame of the bootup animation
 void drawFrame(byte startHue8, int8_t yHueDelta8, int8_t xHueDelta8) {
   byte lineStartHue = startHue8;
   
@@ -97,6 +98,7 @@ void drawFrame(byte startHue8, int8_t yHueDelta8, int8_t xHueDelta8) {
   }
 }
 
+// Reset all pixels of the clock to black
 void resetClock() {
   for (int x = 0; x < WIDTH; x++) {
     for (int y = 0; y < HEIGHT; y++) {
@@ -105,6 +107,7 @@ void resetClock() {
   }
 }
 
+// Draw a single word into the leds array - does not turn on the leds
 void drawWord(int *word, int color) {
   int numWords = *word;
 
@@ -113,16 +116,17 @@ void drawWord(int *word, int color) {
     int x = *(word + currWordOffset + 1);
     int y = *(word + currWordOffset + 2);
     int len = *(word + currWordOffset + 3);
-    
+
     for (int i = 0; i < len; i++) {
       leds[xy(x + i, y)] = CHSV(100, 255, 255);
     }
   }
 }
 
+// Translate (x, y) into an index into the led strip
 uint16_t xy(uint8_t x, uint8_t y) {
   uint16_t i;
-  
+
   if (y & 0x01) {
     uint8_t reverseX = (WIDTH - 1) - x;
     i = (y * WIDTH) + reverseX;
